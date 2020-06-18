96e72bc959e6d8911ff867c33c5705939c194a4d
Dodoma95 <maillard.dorian60@gmail.com>
2020-06-18 22:02:15 +0200

    add mailer 2

--- src/Controller/AdminSecuController.php
+++ src/Controller/AdminSecuController.php
@@ -3,13 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use SendGrid\Mail\Mail;
 use App\Form\SignUpType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AdminSecuController extends AbstractController
 {
@@ -19,17 +21,37 @@ class AdminSecuController extends AbstractController
     public function index(Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder)
     {
         $user = new User();
+        $email = new Mail();
+        $email->setFrom("soresfarm@gmx.fr", "Example User");
+        $email->setSubject("Welcome to the SoresFarm");
+        $email->addContent("text/plain", "Merci d'avoir choisie notre service, vous avez enfin compris ce qu'est la bonne nourriture lol");
+        $email->addContent(
+            "text/html", "<p>Incroyable vous êtes notre premier client</p>"
+        );
+        $sendgrid = new \SendGrid('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');
         $form = $this->createForm(SignUpType::class, $user);
 
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
-            $passwordCrypte = $encoder->encodePassword($user,$user->getPassword());
-            $user->setPassword($passwordCrypte);
-            $user->setRoles('ROLE_USER')->setUpdatedAt(new \DateTime('now'))->setCreatedAt(new \DateTime('now'));
-            $entityManager->persist($user);
-            $entityManager->flush();
-            $this->addFlash("success", "Votre compte a bien été crée");
-            return $this->redirectToRoute('accueil');
+            try {
+                $passwordCrypte = $encoder->encodePassword($user,$user->getPassword());
+                $user->setPassword($passwordCrypte);
+                $user->setRoles('ROLE_USER')->setUpdatedAt(new \DateTime('now'))->setCreatedAt(new \DateTime('now'));
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $this->addFlash("success", "Votre compte a bien été crée");
+                
+                $email->addTo($user->getEmail(), "User just sign up");
+                $response = $sendgrid->send($email);
+                print $response->statusCode() . "\n";
+                print_r($response->headers());
+                print $response->body() . "\n";
+                return $this->redirectToRoute('accueil');
+            } catch (Exception $e) {
+                echo 'Caught exception: '. $e->getMessage() ."\n";
+            }
+
         }
 
         return $this->render('admin_secu/signup.html.twig', [
